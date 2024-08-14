7df340929a942f778a599c4c5813e8363cfaa335
harrytorres <torresharry2@gmail.com>
2024-08-14 12:56:29 +0800

    Refactor book association

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,7 @@ class BooksController < ApplicationController
 
   # POST /books or /books.json
   def create
-    @book = current_user.book.build(book_params)
+    @book = current_user.books.build(book_params)
     if @book.save
       redirect_to @book, notice: 'Book was successfully created.'
     else
